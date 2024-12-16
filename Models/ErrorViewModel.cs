4a546904e32ab40cd375ce6ff024b65df5d484d3
Kristijan Mihalinec <kristijan.mihalinec@king-ict.cloud>
2024-12-16 19:19:40 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_shop_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

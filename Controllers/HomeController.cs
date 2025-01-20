34689ee8b43c35297da5b4a6730be4b1e6a1e642
Kristijan Mihalinec <kristijan.mihalinec@king-ict.cloud>
2025-01-20 19:44:38 +0100

    finished product catalog

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using SQLitePCL;
 using System.Diagnostics;
 using web_shop_app.Data;
 using web_shop_app.Models;
@@ -29,22 +27,37 @@ namespace web_shop_app.Controllers
             return View();
         }
 
-        public IActionResult Products()
+        public IActionResult Products(int? categoryId)
         {
             ViewBag.Categories = _context.Categories.ToList();
 
-            var products = _context.Products.ToList();
+            List<Product> products = new List<Product>();
+
+            if (categoryId == null)
+            {
+                products = _context.Products.ToList();
+            }
+            else
+            {
+                products = (
+                    from product in _context.Products
+                    join product_category in _context.ProductCategories
+                        on product.Id equals product_category.ProductId
+                    where product_category.CategoryId == categoryId
+                     select new Product
+                        {
+                            Id = product.Id,
+                            Title = product.Title,
+                            Description = product.Description,
+                            Quantity = product.Quantity,
+                            Price = product.Price
+                        }
+               ).ToList();
+            }
+
+            
 
             return View(products);
-        }
-
-        [HttpGet]
-        public List<Product> FilterProductsByCategory(int? categoryId)
-        {
-            if(categoryId != null)
-            {
-                return _context.Products.ToList();
-            }
         }
 
 
